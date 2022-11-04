5786e04162fd7fa6334b3c6bead3a2af4804c424
Nemo3003 <ulimarian2000@gmail.com>
2022-11-04 10:14:19 -0300

    [UPD] A small test passed

---
+++ test/spells.test.js
@@ -0,0 +1,12 @@
+
+const request = require( "supertest");
+
+const baseURL = 'http://localhost:8080';
+
+describe("Get all spells", () => {
+    it("should get all spells", async () => {
+        const response = await request(baseURL).get("/spell")
+        expect(response.statusCode).toBe(404);
+        expect(response.body.error).toBe(undefined);
+    });
+  });
