d36f719807d86e60eadce03811e70134baeed085
Eric Burns <eburns000@byui.edu>
2022-11-09 10:50:09 -0800

    update routes->index with users routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const users = require('./users');
+const usersRoute = require('./users');
+const userInfoRoute = require("./userInfo");
 const characters = require('./characters');
 const weapons = require('./weapons');
 const spells = require('./spell.routes');
@@ -10,5 +11,7 @@ router.use('/', require('./swagger'))
 router.use('/weapons', weapons);
 router.use('/characters', characters);
 router.use('/spell', spells)
+router.use("/users", usersRoute);
+router.use("/userinfo", userInfoRoute);
 
 module.exports = router;
