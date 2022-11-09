813e2b7b0455f7ccc50c685d8361df94daa4675d
Eric Burns <eburns000@byui.edu>
2022-11-09 09:04:28 -0800

    add user model

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    identifier: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    givenName: {
+      type: String,
+      required: true,
+    },
+    familyName: {
+      type: String,
+      required: true,
+    },
+    locale: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+    },
+  },
+  { collection: "users", timestamps: true }
+);
+
+module.exports = model("User", userSchema);
