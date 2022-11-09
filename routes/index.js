e470123f4d777ad6774bcbf601fa91dfa35ca5a1
Nemo3003 <ulimarian2000@gmail.com>
2022-11-08 22:41:53 -0300

    [UPD] rest done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const users = require('./users');
+const characters = require('./characters');
+const weapons = require('./weapons');
+const spells = require('./spell.routes');
+
 router.use('/', require('./swagger'))
-router.use('/spell', require('./spell.routes'))
+router.use('/weapons', weapons);
+router.use('/characters', characters);
+router.use('/spell', spells)
 
 module.exports = router;
