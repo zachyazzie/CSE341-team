3bed70f7f90c0b3cf2b8e0e4c6004bf7b6f49bc2
Eric Burns <eburns000@byui.edu>
2022-11-09 10:55:30 -0800

    update weapons with loaduser middleware

--- routes/weapons.js
+++ routes/weapons.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const weaponsController = require('../controllers/weapons');
-
 const router = Router();
+const loadUser = require("../middleware/loadUser");
+
+// require user - middleware - let's load the user before we proceed
+// router.use([loadUser]); -- add this when we want to hook up auth0
 
 router.get('/all', weaponsController.getAllWeapons);
-
 router.get('/one/:postId', weaponsController.getOneWeapon);
-
 router.post('/create', weaponsController.createWeapon);
 
 module.exports = router;
