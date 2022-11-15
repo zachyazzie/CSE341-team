5b3b50600841bbd01eac2990686390360445d8f6
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-11-15 11:26:50 -0300

    [UPD] a few tests added

---
+++ routes/__test__/spells.test.js
@@ -0,0 +1,53 @@
+
+const request = require('supertest');
+const app = require ('../../index');
+
+/**
+ * Testing get all spells
+ */
+
+describe("GET /spell", ()=>{
+  it("respond with json containing a list of all spells", (done) => {
+    request(app)
+      .get("/spell")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  })
+});
+
+describe("GET /spell/:id", () => {
+  it("respond with json containing a single spell", (done) => {
+    request(app)
+      .get("/spell/63644b773931e86af28d94b9")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  })
+});
+/**
+ * Testing POST spell endpoint
+ */
+ describe("POST /spell", () => {
+  it("respond with 201 created", (done) => {
+    const data = {
+      level: "9th",
+      name: "passy",
+      casting_time: "1 Action",
+      duration: "Instantaneous",
+      range: "150ft(30 ft)",
+      attack_save: "CON Save",
+      effect: " Necrotic",
+    };
+    request(app)
+      .post("/spell")
+      .send(data)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  })});
+  
