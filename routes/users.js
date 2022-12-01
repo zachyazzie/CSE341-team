4fbf8618907129e2d644e9267e279494d2d8117e
Eric Burns <eburns000@byui.edu>
2022-11-30 17:21:46 -0800

    uncomment auth for characters, users, weapons

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const usersController = require('../controllers/users');
 const loadUser = require("../middleware/loadUser");
 
 // require user - middleware - let's load the user before we proceed
-// router.use([loadUser]); -- add this when we want to hook up auth0
+router.use([loadUser]);
 
 router.get('/', usersController.getUsers);
 router.get('/:id', usersController.getUserById);
