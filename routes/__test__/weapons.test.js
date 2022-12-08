4728bb8ebbb1709f14b01c136a08068b4a1220e1
Eric Burns <eburns000@byui.edu>
2022-12-07 21:30:39 -0800

    update route test files test for routes

--- routes/__test__/weapons.test.js
+++ routes/__test__/weapons.test.js
@@ -1,54 +1,80 @@
+const weaponsRouter = require("../weapons");
+const routesFor = require("./routesFor"); // see helper function
+const request = require("supertest");
+const app = require("../../index");
 
-const request = require('supertest');
-const app = require ('../../index');
+const routes = routesFor(weaponsRouter);
 
-/**
- * Testing get all weapons
- */
+describe("routes", () => {
+  describe("/", () => {
+    it("supports http GET All", () => {
+      expect(routes["/all"]).toContain("get");
+    });
 
-describe("GET /weapons/all", ()=>{
-  it("respond with json containing a list of all weapons", (done) => {
-    request(app)
+    it("supports http POST Create", () => {
+      expect(routes["/create"]).toContain("post");
+    });
+
+    it("supports http GET postId", () => {
+      expect(routes["/one/:postId"]).toContain("get");
+    });
+
+    it("supports http PUT postId", () => {
+      expect(routes["/:postId"]).toContain("put");
+    });
+
+    it("supports http DELETE postId", () => {
+      expect(routes["/:postId"]).toContain("delete");
+    });
+  });
+});
+
+describe("GET /weapons/all", () => {
+  it("respond with json content type and 200 status", async () => {
+    const response = await request(app)
       .get("/weapons/all")
       .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
-  })
+      .set('Authorization', 'bearer ' + process.env.ACCESS_TOKEN);
+    expect(response.headers["content-type"]).toEqual(
+      "application/json; charset=utf-8"
+    );
+    expect(response.status).toEqual(200);
+  });
 });
 
-describe("GET /weapons/one/:id", () => {
-  it("respond with json containing a single weapon", (done) => {
-    request(app)
-      .get("/weapons/one/6364104eee3a4185c678ca2a")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
-  })
-});
-/**
- * Testing POST spell endpoint
- */
- describe("POST /weapons/create", () => {
-  it("respond with 201 created", (done) => {
-    const data = {
-      weaponName: "urling",
-      description: "passy",
-      category: "Action",
-      damage: "high",
-      damageType: "Neurotic",
-      itemRarity: "Rare",
-      properties: "cool",
-      weight: 3
-    };
-    request(app)
-      .post("/weapons/create", {
-        data
-      })
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(201)
-      .end((err) => {
-        if (err) return done(err);
-        done();
-      });
-  })});
+// describe("GET /weapons/one/:id", () => {
+//   it("respond with json containing a single weapon", (done) => {
+//     request(app)
+//       .get("/weapons/one/6364104eee3a4185c678ca2a")
+//       .set("Accept", "application/json")
+//       .expect("Content-Type", /json/)
+//       .expect(200, done);
+//   })
+// });
+// /**
+//  * Testing POST spell endpoint
+//  */
+//  describe("POST /weapons/create", () => {
+//   it("respond with 201 created", (done) => {
+//     const data = {
+//       weaponName: "urling",
+//       description: "passy",
+//       category: "Action",
+//       damage: "high",
+//       damageType: "Neurotic",
+//       itemRarity: "Rare",
+//       properties: "cool",
+//       weight: 3
+//     };
+//     request(app)
+//       .post("/weapons/create", {
+//         data
+//       })
+//       .set("Accept", "application/json")
+//       .expect("Content-Type", /json/)
+//       .expect(201)
+//       .end((err) => {
+//         if (err) return done(err);
+//         done();
+//       });
+//   })});
